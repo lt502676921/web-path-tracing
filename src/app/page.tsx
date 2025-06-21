25dd17290b86aba21767239e50495ef7a1e22b33
oreo <Tang.Li5@geely.com>
2025-06-21 12:53:56 +0800

    fix: webgl2.0 text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <Link href="/webgl">WebGL 1.0</Link>
         </button>
         <button className="w-48 sm:w-32 cursor-pointer bg-gray-800 relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-gray-700 text-gray-300 hover:text-blue-400 h-9 rounded-md px-3">
-          <Link href="/webgl2">WebGL 12.0</Link>
+          <Link href="/webgl2">WebGL 2.0</Link>
         </button>
       </div>
     </main>
